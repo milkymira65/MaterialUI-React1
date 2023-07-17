c5a966e2abbe6dcfc1bf92511f95389b6550acd2
milkymira65 <milkymirataewana@gmail.com>
2023-07-18 01:24:49 +0700

    update-navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Rightbar from "./component/Rightbar";
 import Feed from "./component/Feed";
 import Sidebar from "./component/Sidebar";
 import { Box, Stack } from "@mui/material";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <Box>
-      {/* Navber */}
+      <Navbar />
       <Stack direction="row" spacing={2} justifyContent="space-between">
         <Sidebar />
         <Feed />
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,49 @@
+import { AppBar ,Box,Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import styled from 'styled-components'
+import UnstyledInputBasic from "./inputSarch-fornav"
+import Badge from '@mui/base/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+// import AccessibleBadges from "./mail-fornav"
+
+
+const StyledToolbar = styled(Toolbar)({
+  display:"flex",
+  justifyContent:"space-between"
+
+})
+
+//  const Search =styled("div")(({theme}) =>({
+//   backgroundColor: "white",
+//  padding:"0 10px",
+//  borderRadius: "5px",
+//  width:"40%"
+//  }))
+
+ const Icons =styled(Box)(({theme}) =>({
+  // backgroundColor: "white",
+  //  borderRadius: "5px",
+  //  padding:"0 2px",
+ }))
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <AppBar position='sticky'>
+      <StyledToolbar>
+        <Typography  variant='h6' sx={{display:{xs:"none",sm:"block" }}} > 
+MILKY MIRA
+      </Typography>
+  
+     <UnstyledInputBasic/>
+  
+       <Icons>
+       <Badge badgeContent={4}  >
+  <MailIcon/>
+</Badge>
+       </Icons>
+    </StyledToolbar>
+     
+    </AppBar>
   )
 }
 
